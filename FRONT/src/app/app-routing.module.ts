6fc430dddf526065f692a003e12a87731582743d
Felipe Silva <lipe1398@gmail.com>
2021-09-28 22:10:51 -0300

    fim do front, deu certo!!!!

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CadastrarAutomovelComponent } from "./components/views/automovel/cadastrar-automovel/cadastrar-automovel.component";
+import { ListarAutomovelComponent } from "./components/views/automovel/listar-automovel/listar-automovel.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "",
+        component: ListarAutomovelComponent,
+    },
+    {
+        path: "automovel/listar",
+        component: ListarAutomovelComponent,
+    },
+    {
+        path: "automovel/cadastrar",
+        component: CadastrarAutomovelComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
